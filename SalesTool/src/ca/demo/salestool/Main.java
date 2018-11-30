544f2bd8dabb83733845710fa4c363e063e11868
Ram_Chintapalli <Ram_Chintapalli@EPRUPETW9422.PETERSBURG.epam.com>
2018-11-30 18:23:45 +0530

    added initial stuff

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,17 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+				data.display();
+				displayGreeting();
+
+	}
+	public static void displayGreeting() {
+		System.out.println("happy sales folks");
+		System.out.println("displays sales data");
+		System.out.println("sslsl");
+	}
+
+}
---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,10 @@
+package ca.demo.salestool;
+
+public class SalesData {
+	int data [] = {2,4,6};
+	public void display() {
+		System.out.println("TODO: Data display       ");
+	
+}
+
+}
